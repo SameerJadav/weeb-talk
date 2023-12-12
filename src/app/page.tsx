755e22d7bd688f5b9b5e8059aea0fdd7a57b0097
SameerJadav <sameerjadav010@gmail.com>
2023-12-12 17:44:05 +0530

    feat: basic next auth setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-export default function HomePage() {
+import { redirect } from "next/navigation";
+import { auth } from "~/server/auth";
+
+export default async function HomePage() {
+  const session = await auth();
+
+  if (!session?.user) {
+    redirect("api/auth/signin?callbackUrl=/");
+  }
+
   return (
     <main>
-      <p>HomePage</p>
+      <p>{session.user.name}</p>
     </main>
   );
 }
